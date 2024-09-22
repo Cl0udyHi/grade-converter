8ca26ed15b103ff4ea25a9b42b965ab863ba5a06
CloudyHi <58792943+Cl0udyHi@users.noreply.github.com>
2024-09-22 13:38:04 +0100

    update!

--- script.js
+++ script.js
@@ -1,29 +1,85 @@
 const outputElement = document.getElementById("output");
 
-function getGrade(input) {
-  if (input >= 95 && input <= 100) output = "A+";
-  if (input >= 90 && input <= 94) output = "A";
-  if (input >= 85 && input <= 89) output = "B+";
-  if (input >= 80 && input <= 84) output = "B";
-  if (input >= 75 && input <= 79) output = "C+";
-  if (input >= 75 && input <= 74) output = "C";
-  if (input >= 65 && input <= 69) output = "D+";
-  if (input >= 60 && input <= 64) output = "D";
-  if (input < 60) output = "F";
+function reset() {
+  outputElement.classList.remove("a");
+  outputElement.classList.remove("b");
+  outputElement.classList.remove("c");
+  outputElement.classList.remove("d");
+  outputElement.classList.remove("f");
+}
 
-  return output;
+function setGrade(input) {
+  let output = "F";
+
+  if (input < 0) outputElement.textContent = "e";
+
+  if (input >= 95 && input <= 100) {
+    output = "A+";
+    reset();
+    outputElement.classList.add("a");
+  }
+  if (input >= 90 && input <= 94) {
+    output = "A";
+    reset();
+    outputElement.classList.add("a");
+  }
+  if (input >= 85 && input <= 89) {
+    output = "B+";
+    reset();
+    outputElement.classList.add("b");
+  }
+  if (input >= 80 && input <= 84) {
+    output = "B";
+    reset();
+    outputElement.classList.add("b");
+  }
+  if (input >= 75 && input <= 79) {
+    output = "C+";
+    reset();
+    outputElement.classList.add("c");
+  }
+  if (input >= 70 && input <= 74) {
+    output = "C";
+    reset();
+    outputElement.classList.add("c");
+  }
+  if (input >= 65 && input <= 69) {
+    output = "D+";
+    reset();
+    outputElement.classList.add("d");
+  }
+  if (input >= 60 && input <= 64) {
+    output = "D";
+    reset();
+    outputElement.classList.add("d");
+  }
+  if (input < 60) {
+    output = "F";
+    reset();
+    outputElement.classList.add("f");
+  }
+
+  outputElement.classList.add("f");
+
+  outputElement.textContent = output;
 }
 
 window.onload = function () {
-  outputElement.textContent = getGrade(0);
+  reset();
+  return (outputElement.textContent = "?");
 };
 
 document.addEventListener("input", (e) => {
   const input = e.target.value;
-  let output = getGrade(input);
 
-  if (input < 0) e.target.value = 0;
-  if (input > 100) e.target.value = 100;
+  if (!input) {
+    reset();
+    return (outputElement.textContent = "?");
+  }
 
-  outputElement.textContent = output;
+  if (input < 0 || input > 100) {
+    e.target.value = 0;
+  }
+
+  setGrade(input);
 });
